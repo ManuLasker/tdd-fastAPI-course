e1d6ad351f811c2c038eef86cefc201e9b059409
ManuLasker <emanuel.afanador@koombea.com>
2021-06-09 17:28:24 -0500

    :umbrella:  add test coverage for endpoints

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -2,8 +2,8 @@ import os
 from typing import Generator
 import pytest
 from starlette.testclient import TestClient
-
-from app import main
+from tortoise.contrib.fastapi import register_tortoise
+from app.main import create_app
 from app.config import get_settings, Settings
 
 
@@ -15,9 +15,27 @@ def get_settings_override() -> Settings:
 def test_app() -> Generator:
     # set up
     # override app dependencei get_settings with test settings define previously
-    main.app.dependency_overrides[get_settings] = get_settings_override
+    app = create_app()
+    app.dependency_overrides[get_settings] = get_settings_override
     
-    with TestClient(main.app) as test_client:
+    with TestClient(app) as test_client:
         # testing
+        yield test_client
+    # tear down
+
+@pytest.fixture(scope="module")
+def test_app_with_db() -> Generator:
+    # set up
+    app = create_app()
+    app.dependency_overrides[get_settings] = get_settings_override
+    register_tortoise(
+        app,
+        db_url=get_settings_override().database_url,
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=True,
+        add_exception_handlers=True
+    )
+    # create test client
+    with TestClient(app) as test_client:
         yield test_client
     # tear down
