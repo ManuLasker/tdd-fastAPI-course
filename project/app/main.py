cbbe5bc9ddc386f7b2b71a9e4f96685735b18f62
ManuLasker <emanuel.afanador@koombea.com>
2021-06-09 13:08:18 -0500

    :newspaper: main app file

--- project/app/main.py
+++ project/app/main.py
@@ -1,8 +1,18 @@
+import os
 from fastapi import FastAPI, Depends
 from app.config import get_settings, Settings
+from tortoise.contrib.fastapi import register_tortoise
 
 app = FastAPI()
 
+# This register tortoise models and initialize db to fastapi
+register_tortoise(
+    app,
+    db_url=get_settings().database_url,
+    modules={"models": ["app.models.tortoise"]},
+    generate_schemas=True,
+    add_exception_handlers=True
+)
 
 @app.get("/ping")
 async def ping(settings: Settings = Depends(get_settings)):
