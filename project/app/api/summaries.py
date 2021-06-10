1c0ea3eee984156292927601545c9651cea4dfab
ManuLasker <emanuel.afanador@koombea.com>
2021-06-10 18:05:55 -0500

    :sparkles: added automatic summarization background task to url post create

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -1,7 +1,8 @@
 from typing import List
 
 from app.api import crud
-from fastapi import APIRouter, HTTPException, Path
+from app.summarizer import generate_summary
+from fastapi import APIRouter, BackgroundTasks, HTTPException, Path
 
 from app.models.pydantic import (  # isort:skip
     SummaryPayloadSchema,
@@ -14,9 +15,11 @@ router = APIRouter()
 
 
 @router.post("/", response_model=SummaryResponseSchema, status_code=201)
-async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema:
+async def create_summary(
+    payload: SummaryPayloadSchema, background_tasks: BackgroundTasks
+) -> SummaryResponseSchema:
     summary_id = await crud.post(payload)
-
+    background_tasks.add_task(generate_summary, summary_id, payload.url)
     response_object = {"id": summary_id, "url": payload.url}
 
     return response_object
